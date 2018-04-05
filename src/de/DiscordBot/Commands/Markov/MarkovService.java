7f34c1d95c9893677ab914032ec0ade073d0f207
Maximilian Kling <kling-maximilian@protonmail.com>
2018-04-05 13:34:54 +0200

    Fixed MarkovService Initialization

--- src/de/DiscordBot/Commands/Markov/MarkovService.java
+++ src/de/DiscordBot/Commands/Markov/MarkovService.java
@@ -17,59 +17,81 @@ public class MarkovService extends DiscordService {
 	Markov global;
 	ListenerAdapter listener;
 	private static MarkovService service;
-	
+	private boolean ready = false;
+
 	@Override
 	public void run() {
 		global = new Markov();
 		ChatLog cl = CommandExecutor.getChatLog();
-		for(Guild g : DiscordBot.getBot().getGuilds()) {
-			for(ChatLogChannel clc : cl.listChannels(g).values()) {
+		for (Guild g : DiscordBot.getBot().getGuilds()) {
+			for (ChatLogChannel clc : cl.listChannels(g).values()) {
 				clc.load();
-				for(ChatLogMessage clm : clc.clm) {
+				for (ChatLogMessage clm : clc.clm) {
 					String content = clm.content;
-					if(content.startsWith("!")||content.startsWith("t!")||content.startsWith("~")||content.startsWith("\\")||content.startsWith("/")||content.startsWith("-")) {
+					if (content.startsWith("!") || content.startsWith("t!") || content.startsWith("~")
+							|| content.startsWith("\\") || content.startsWith("/") || content.startsWith("-")) {
 						continue;
 					}
 					global.addWords(content);
 				}
 			}
 		}
-		
-		DiscordBot.getBot().addEventListener((listener=new ListenerAdapter() {
+
+		DiscordBot.getBot().addEventListener((listener = new ListenerAdapter() {
 			@Override
 			public void onMessageReceived(MessageReceivedEvent event) {
-				if(event.getChannelType()==ChannelType.TEXT) {
-					if(!event.getAuthor().isBot()) {
+				if (event.getChannelType() == ChannelType.TEXT) {
+					if (!event.getAuthor().isBot()) {
 						Message m = event.getMessage();
 						String content = m.getContent();
-						if(!content.isEmpty()) {
-							if(content.startsWith("!")||content.startsWith("t!")||content.startsWith("~")||content.startsWith("\\")||content.startsWith("/")||content.startsWith("-")) {
+						if (!content.isEmpty()) {
+							if (content.startsWith("!") || content.startsWith("t!") || content.startsWith("~")
+									|| content.startsWith("\\") || content.startsWith("/") || content.startsWith("-")) {
 								return;
 							}
 							global.addWords(content);
 						}
 					}
 				}
-			}			
+			}
 		}));
+		ready = true;
 	}
 
 	public static String generateSentence(String seed) {
-		if(service == null) {
+		if (service == null) {
 			service = new MarkovService();
 			DiscordBot.startService(service);
+			try {
+				service.waitTillReady();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		return service.global.generateSentence(seed);
 	}
-	
+
+	private void waitTillReady() throws InterruptedException {
+		while (!ready) {
+			Thread.sleep(500L);
+		}
+	}
+
 	public static String generateSentence() {
-		if(service == null) {
+		if (service == null) {
 			service = new MarkovService();
 			DiscordBot.startService(service);
+			try {
+				service.waitTillReady();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		return service.global.generateSentence();
 	}
-	
+
 	@Override
 	public void shutdown() {
 		service = null;
