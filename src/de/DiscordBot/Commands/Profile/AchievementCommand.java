4b79b52950e76b85c94595a5f9eaf68a43e669bd
Maximilian Kling <kling-maximilian@protonmail.com>
2018-04-25 12:51:23 +0200

    Updated AchievementManager and implemented AchievementCommand

--- src/de/DiscordBot/Commands/Profile/AchievementCommand.java
+++ src/de/DiscordBot/Commands/Profile/AchievementCommand.java
@@ -1,22 +1,50 @@
 package de.DiscordBot.Commands.Profile;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import de.DiscordBot.Commands.DiscordCommand;
+import de.DiscordBot.Commands.Profile.Achievements.AchievementManager;
 import de.DiscordBot.Config.Config;
 import de.DiscordBot.Config.ConfigPage;
+import net.dv8tion.jda.core.MessageBuilder;
 import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.User;
 
 public class AchievementCommand extends DiscordCommand {
 
 	public AchievementCommand() {
-		super("achievements", new String[] {}, "Let's you look at your achievements (there are many hidden ones)", "\\achievements [page]");
+		super("achievements", new String[] {}, "Lets you look at your achievements (there are many hidden ones)",
+				"\\achievements [page]");
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public Object execute(String command, String[] args, Message m) {
-		
-		return null;
+		User u = m.getAuthor();
+		List<String> achievements = AchievementManager.listAchievedAchievements(u);
+		int page = 1;
+		if (args.length > 0) {
+			try {
+				page = Integer.valueOf(args[0]);
+			} catch (Exception e) {
+				return new MessageBuilder().append("You need to specify a valid Page Number!").build();
+			}
+		}
+		int start = (page-1) * 8;
+		if(achievements.size()<start+1) {
+			return new MessageBuilder().append("You don't have that many achievements...").build();
+		}
+		MessageBuilder mb = new MessageBuilder();
+		mb.append("Achievement List:\nPage " + page);
+		String display = String.join("\n", achievements.subList(start, start+8));
+		mb.appendCodeBlock(display, null);
+		mb.append("Pages: ");
+		for(int i = 1; i-1 < (double) achievements.size() / 8.0; i++) {
+			mb.append(i + " ");
+		}
+		return mb.build();
 	}
 
 	@Override
--- src/de/DiscordBot/Commands/Profile/Achievements/AchievementManager.java
+++ src/de/DiscordBot/Commands/Profile/Achievements/AchievementManager.java
@@ -1,7 +1,13 @@
 package de.DiscordBot.Commands.Profile.Achievements;
 
+import java.net.URLDecoder;
 import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import org.apache.commons.collections4.map.LinkedMap;
 
@@ -14,30 +20,39 @@ import net.dv8tion.jda.core.entities.MessageChannel;
 import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.entities.User;
 
-
 public class AchievementManager {
 
 	static LinkedMap<Achievement, Boolean> achievements = new LinkedMap<Achievement, Boolean>();
-	
+
 	static {
 		new CommandAchievement();
 	}
-	
-	
-	
+
 	public static void processMessage(Message m, Config c) {
-		for(Achievement a : achievements.keySet()) {
-			if(achievements.get(a)) {
-			a.process(m, c);
+		for (Achievement a : achievements.keySet()) {
+			if (achievements.get(a)) {
+				a.process(m, c);
 			}
 		}
 	}
-	
+
+	public static List<String> listAchievedAchievements(User u) {
+		Pattern p = Pattern.compile("^achievement_" + u.getId() + "_(.*)$");
+		ArrayList<String> keys = ProfileCommand.config.getKeys(p.pattern());
+		Map<String, Boolean> bools = ProfileCommand.config.getBooleanValues(keys.toArray(new String[keys.size()]));
+		
+		return bools.entrySet().stream()
+				.filter((Map.Entry<String, Boolean> entry) -> entry.getValue())
+				.map((Map.Entry<String, Boolean> entry) -> URLDecoder.decode(p.matcher(entry.getKey()).group(1)))
+				.collect(Collectors.toList());
+	}
+
 	public static void achievedAchievement(MessageChannel tc, User u, String achievement) {
 		ProfileCommand.config.setValue("achievement_" + u.getId() + "_" + URLEncoder.encode(achievement), true);
 		MessageBuilder mb = new MessageBuilder();
-		Message m = mb.append("Hoooray ").append(u).append(", you unlocked the Achievement \"" + achievement + "\" :smile:").build();
+		Message m = mb.append("Hoooray ").append(u)
+				.append(", you unlocked the Achievement \"" + achievement + "\" :smile:").build();
 		tc.sendMessage(m).queue();
 	}
-	
+
 }
